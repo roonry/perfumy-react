b711e109547dd6d088fc853f41d5788dc732f20f
roonry <rooneypriya9@gmail.com>
2024-12-18 10:07:30 +0400

    ubdated perfumy

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+export default Navbar
+
+// Navbar
+function Navbar() {
+    return (
+      <nav>
+        <h1>Perfumy</h1>
+        <ul>
+          <li>Home|</li>
+          <li>Products|</li>
+          <li>About|</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    )
+  }
---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+export default Search
+
+// Search
+function Search() {
+    return (
+      <div className="searchbar">
+        <input type="text" placeholder="Search" />
+      </div>
+    )
+  }
